3f4b563ff77f19e89629fe1edbda2b97df5b1f37
Igor <42452520+quasimodus@users.noreply.github.com>
2020-08-30 23:00:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function conv_mac() {
+    var text1 = document.getElementById("text_in").value;
+    var text2 = document.getElementById("text_out").value;
+    if (text1 != text2) {
+        document.getElementById("text_out").append(text1);
+    }else{
+        
+    }
+
+
+}
+
+
